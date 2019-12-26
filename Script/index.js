b0cdd01697f635dd20880447013e07b7247959c2
xiaoniutongxue <1991772345@qq.com>
2019-12-26 19:49:37 +0800

    '增加按钮'

--- Script/index.js
+++ Script/index.js
@@ -2,7 +2,19 @@ new Vue({
     el:"#app",
     data:function(){
         return{
-            input:''
+            input:'',
+            userInfo:{
+                name:'',
+                gander:'',
+                phoneNum:'',
+                birthDay:''
+            },
+            tableData: [{
+                name:'王小虎',
+                gander:'男',
+                phoneNum:'13142135083',
+                birthDay:'20191202'
+            }]
         }
     },
     methods:{
